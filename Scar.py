45c79c0678d6d50b4a88613890340ec7ee25574b
SkabeW <skabwan@gmail.com>
2018-07-27 20:26:02 +0300

    longpoll is almost ready

--- Scar.py
+++ Scar.py
@@ -21,11 +21,6 @@ def main():
         print(error_msg)
         return
 
-    # Авторизация группы (для групп рекомендуется использовать VkBotLongPoll):
-    # при передаче token вызывать vk_session.auth не нужно
-    
-    #vk_session = vk_api.VkApi(token='d74b56a9d74b56a9d74b56a915d72e322ddd74bd74b56a98c10673b82fd8829405ee408')
-
     vk = vk_session.get_api()
 
     upload = VkUpload(vk_session)  # Для загрузки изображений
@@ -36,8 +31,12 @@ def main():
             peer_id = id,
             message = s
         )
+        
     write(TEST_CONFA, 'suka')
-
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            print('id{}: "{}"'.format(event.user_id, event.text), end=' ')
+            write(TEST_CONFA, 'Миша ' + event.text + '? а с ебалом че?')
 
 
 if __name__ == '__main__':
